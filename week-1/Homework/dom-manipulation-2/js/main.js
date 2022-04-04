a2609ca2f857b4084d7e0d2300526abc1ebd2562
Jamesty7 <jamestysoto@gmail.com>
2022-04-04 21:52:53 +0200

    Week 1 Part 2 dom manipulation 2 done

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -1,5 +1,9 @@
+// Part 1
+
+// Blue Button
+
 const changeColorBlueBtn = document.querySelector("#blueBtn")
- 
+
 changeColorBlueBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
@@ -18,7 +22,7 @@ changeColorBlueBtn.addEventListener("click", (event) => {
 // Orange Button
 
 const changeColorOrangeBtn = document.querySelector("#orangeBtn")
- 
+
 changeColorOrangeBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
@@ -37,7 +41,7 @@ changeColorOrangeBtn.addEventListener("click", (event) => {
 // Green Button
 
 const changeColorGreenBtn = document.querySelector("#greenBtn")
- 
+
 changeColorGreenBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
@@ -50,7 +54,55 @@ changeColorGreenBtn.addEventListener("click", (event) => {
     // Volunteer button background color to black and text color to white
     const volunteerButton = document.querySelector(".jumbotron .btn-secondary")
     volunteerButton.style.background = "#8c9c08"
-    
+
+})
+
+
+// Part 2
+
+// Display an alert to thank you for filling out the form
+// Blank out (make empty) all the text fields
+
+const submitButton = document.querySelector("form button")
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+   
+
+    // Your name, Email and Describe yourself fields
+    const emailInput = document.querySelector("#exampleInputEmail1")
+    const email = emailInput.value
+    const isEmailValid = email.length > 0 && email.includes("@")
+    if (isEmailValid === false) {
+        emailInput.style.background = "red"
+    } else {
+        emailInput.style.background = "white"
+    }
+
+    const nameInput = document.querySelector("#example-text-input")
+    const name = nameInput.value
+    const isNameValid = name.length > 0
+    if (isNameValid === false) {
+        nameInput.style.background = "red"
+    } else {
+        nameInput.style.background = "white"
+    }
+
+    const personalDescriptionInput = document.querySelector("#exampleTextarea")
+    const personalDescription = personalDescriptionInput.value
+    const isPersonalDescriptionValid = personalDescription.length > 0
+    if (isPersonalDescriptionValid === false) {
+        personalDescriptionInput.style.background = "red"
+    } else {
+        personalDescriptionInput.style.background = "white"
+    }
+
+    const isEverythingValid = isEmailValid && isNameValid && isPersonalDescriptionValid 
+    if (isEverythingValid) {
+        window.alert("Thank you for filling out the form") 
+        emailInput.value=''
+        nameInput.value=''
+        personalDescriptionInput.value=''
+    }
 })
 
 
