1615e9c412b4fe160e81e55d178e03a6adbb91df
Jamesty7 <jamestysoto@gmail.com>
2022-04-23 15:32:56 +0200

    obj homework update

--- week-2/Homework/Part-2/G-object-loops/exercise-1.js
+++ week-2/Homework/Part-2/G-object-loops/exercise-1.js
@@ -6,8 +6,23 @@ const studentGrades = {
     tom: 20,
     george: 17,
     abdul: 19,
-  };
-  
+}
+
+let myKey = "abba"
+let tomValue = studentGrades.tom
+let tomValue2 = studentGrades["tom"]
+studentGrades[myKey]
+
+for (const prop in studentGrades) {
+  let grade = studentGrades[prop]
+  if (grade >= 18) { 
+    console.log(prop)
+  }
+}
+   
+ 
+
+
   // Prints
   // TOM - 20
   // ABDUL - 19
